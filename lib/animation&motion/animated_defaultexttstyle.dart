7bc3ee3c1662ecf49adcf176153a4201e2e661cb
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-04 23:18:27 +0100

    Create animated_defaultexttstyle.dart

---
+++ lib/animation&motion/animated_defaultexttstyle.dart
@@ -0,0 +1,3 @@
+// Constructors
+// AnimatedDefaultTextStyle({Key? key, required Widget child, required TextStyle style, TextAlign? textAlign, bool softWrap = true, TextOverflow overflow = TextOverflow.clip, int? maxLines, TextWidthBasis textWidthBasis = TextWidthBasis.parent, TextHeightBehavior? textHeightBehavior, Curve curve = Curves.linear, required Duration duration, VoidCallback? onEnd})
+// Creates a widget that animates the default text style implicitly

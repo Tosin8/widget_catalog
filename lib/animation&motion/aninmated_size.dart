3fb1efa14c8a51c3ea648d8636a6ca89b0143009
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-05 00:53:02 +0100

    Adding Animated Size

---
+++ lib/animation&motion/aninmated_size.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Animated_Sizing extends StatefulWidget {
+  const Animated_Sizing({super.key});
+
+  @override
+  State<Animated_Sizing> createState() => _Animated_SizingState();
+}
+
+class _Animated_SizingState extends State<Animated_Sizing> {
+  bool _large = false;
+  final double _size = 50.0;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _large = !_large;
+        });
+      },
+      child: Container(
+        color: Colors.amberAccent,
+        child: const AnimatedSize(
+          duration: Duration(seconds: 1),
+          curve: Curves.easeIn,
+          child: FlutterLogo(size: _size),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:widget_catalog/animation&motion/animated_position.dart';
+
+import 'animation&motion/aninmated_size.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const Animated_Positioning(),
+      home: const Animated_Sizing(),
     );
   }
 }

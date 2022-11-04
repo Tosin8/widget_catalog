eec64b197d6da7900a31fe084b1979097cb5f674
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-04 21:30:18 +0100

    first commit

--- lib/animation&motion/animated_align.dart
+++ lib/animation&motion/animated_align.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Animated_Align extends StatefulWidget {
+  const Animated_Align({super.key});
+
+  @override
+  State<Animated_Align> createState() => _Animated_AlignState();
+}
+
+class _Animated_AlignState extends State<Animated_Align> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

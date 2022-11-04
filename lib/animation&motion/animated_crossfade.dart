3fa7f6f095ea76e121144ad5c69cce4974980b56
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-04 23:16:02 +0100

    Update animated_crossfade.dart

--- lib/animation&motion/animated_crossfade.dart
+++ lib/animation&motion/animated_crossfade.dart
@@ -11,21 +11,32 @@ class _animated_crossfadingState extends State<animated_crossfading> {
   bool selected = false;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selected = !selected;
-        });
-      },
-      child: AnimatedCrossFade(
-        duration: const Duration(seconds: 3),
-        firstCurve: Curves.easeOut,
-        secondCurve: Curves.easeIn,
-        sizeCurve: Curves.bounceOut,
-        firstChild: const Text('hello'),
-        secondChild: const Text('goodbye'),
-        crossFadeState:
-            selected ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            selected = !selected;
+          });
+        },
+        child: Center(
+          child: AnimatedCrossFade(
+            duration: const Duration(seconds: 3),
+            firstCurve: Curves.easeOut,
+            secondCurve: Curves.easeIn,
+            sizeCurve: Curves.bounceOut,
+            firstChild: const Text('hello',
+                style: TextStyle(fontSize: 30, color: Colors.green)),
+            secondChild: const Text(
+              'goodbye',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.blue,
+              ),
+            ),
+            crossFadeState:
+                selected ? CrossFadeState.showFirst : CrossFadeState.showSecond,
+          ),
+        ),
       ),
     );
   }

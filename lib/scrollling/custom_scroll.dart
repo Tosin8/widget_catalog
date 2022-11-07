e1c28847f2a31b19b9155c6483c188571c40c64b
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-07 19:25:29 +0100

    Update custom_scroll.dart

--- lib/scrollling/custom_scroll.dart
+++ lib/scrollling/custom_scroll.dart
@@ -17,9 +17,34 @@ class _Custom_ScrollWidgState extends State<Custom_ScrollWidg> {
 
   @override
   Widget build(BuildContext context) {
+    const Key centerKey = ValueKey<String>('bottom-sliver-list');
     return Scaffold(
-      appBar:AppBar(title: const Text('Press on the plus to add items above and below'; 
-      leading: IconButton(icon: Icon(Icons.add), 
-      onPressed: ()) )) ,)
+        appBar: AppBar(
+            title: const Text('Press on the plus to add items above and below'),
+            leading: IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  setState(() {
+                    top.add(-top.length - 1);
+                    bottom.add(bottom.length);
+                  });
+                })),
+        body: CustomScrollView(
+          center: centerKey,
+          slivers: [
+            SliverList(
+                delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.blue[200 + bottom[index] % 4 * 100],
+                  height: 100 + bottom[index] % 4 * 20.0,
+                  child: Text('Item: ${bottom[index]}'),
+                );
+              },
+              childCount: bottom.length,
+            ))
+          ],
+        ));
   }
 }

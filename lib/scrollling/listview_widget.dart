91cffbb6927749ad0d70fae9c123d0fe5ccba34e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-08 10:31:35 +0100

    Updating with listView.separator

--- lib/scrollling/listview_widget.dart
+++ lib/scrollling/listview_widget.dart
@@ -40,30 +40,64 @@
 
 // LISTVIEW BUILDER
 
+// import 'package:flutter/material.dart';
+
+// class LsitView_Widgs extends StatefulWidget {
+//   const LsitView_Widgs({super.key});
+
+//   @override
+//   State<LsitView_Widgs> createState() => _LsitView_WidgsState();
+// }
+
+// class _LsitView_WidgsState extends State<LsitView_Widgs> {
+//   final List<String> entries = ['A', 'B', 'C'];
+//   final List<int> colorCodes = [600, 500, 100];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ListView.builder(
+//           padding: const EdgeInsets.all(8),
+//           itemCount: entries.length,
+//           itemBuilder: (BuildContext context, int index) {
+//             return Container(
+//               height: 50,
+//               color: Colors.amber[colorCodes[index]],
+//               child: Center(child: Text('Entry ${entries[index]}')),
+//             );
+//           }),
+//     );
+//   }
+// }
+
+// LISTVIEW.SEPARATOR
 import 'package:flutter/material.dart';
 
-class LsitView_Widgs extends StatefulWidget {
-  const LsitView_Widgs({super.key});
+class ListView_Builder extends StatefulWidget {
+  const ListView_Builder({super.key});
 
   @override
-  State<LsitView_Widgs> createState() => _LsitView_WidgsState();
+  State<ListView_Builder> createState() => _ListView_BuilderState();
 }
 
-class _LsitView_WidgsState extends State<LsitView_Widgs> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
-
+class _ListView_BuilderState extends State<ListView_Builder> {
+  final List<String> entries = ['A', 'B', 'C'];
+  final List<int> colorCodes = [600, 500, 100];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: entries.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 50,
-            color: Colors.amber[colorCodes[index]],
-            child: Center(child: Text('Entry ${entries[index]}')),
-          );
-        });
+    return Scaffold(
+        body: ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: entries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Container(
+                height: 50,
+                color: Colors.amber[colorCodes[index]],
+                child: Center(child: Text('Entry ${entries[index]}')),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return const Divider();
+            }));
   }
 }

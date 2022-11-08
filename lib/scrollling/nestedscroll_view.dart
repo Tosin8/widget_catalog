bb81860865149ac91d173f8af3fdea6ae4ff7613
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-08 11:42:43 +0100

    Update nestedscroll_view.dart

--- lib/scrollling/nestedscroll_view.dart
+++ lib/scrollling/nestedscroll_view.dart
@@ -86,6 +86,43 @@
 //   }
 // }
 
+// PINNED AND FLOATING SLIVERAPPBARS ( SLIVERAPPBARS WITH NESTEDSCROLLVIEWS)
+
+// import 'package:flutter/material.dart';
+
+// class MyStatelessWidget extends StatelessWidget {
+//   const MyStatelessWidget({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: NestedScrollView(
+// // Setting floatHeaderSlivers to true is required in order to float the outer slivers over the inner scrollable.
+//             floatHeaderSlivers: true,
+//             headerSliverBuilder:
+//                 (BuildContext context, bool innerBoxIsScrolled) {
+//               return <Widget>[
+//                 SliverAppBar(
+//                   title: const Text('Floating Nested SliverAppBar'),
+//                   floating: true,
+//                   expandedHeight: 150.0,
+//                   forceElevated: innerBoxIsScrolled,
+//                 ),
+//               ];
+//             },
+//             body: ListView.builder(
+//                 padding: const EdgeInsets.all(8),
+//                 itemCount: 30,
+//                 itemBuilder: (BuildContext context, int index) {
+//                   return SizedBox(
+//                     height: 50,
+//                     child: Center(child: Text('Item $index')),
+//                   );
+//                 })));
+//   }
+// }
+
+//SNAPPING SILVERAPPBAR.
 import 'package:flutter/material.dart';
 
 class MyStatelessWidget extends StatelessWidget {
@@ -94,29 +131,36 @@ class MyStatelessWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: NestedScrollView(
-            // Setting floatHeaderSlivers to true is required in order to float
-            // the outer slivers over the inner scrollable.
-            floatHeaderSlivers: true,
-            headerSliverBuilder:
-                (BuildContext context, bool innerBoxIsScrolled) {
-              return <Widget>[
-                SliverAppBar(
-                  title: const Text('Floating Nested SliverAppBar'),
-                  floating: true,
-                  expandedHeight: 150.0,
-                  forceElevated: innerBoxIsScrolled,
-                ),
-              ];
-            },
-            body: ListView.builder(
-                padding: const EdgeInsets.all(8),
-                itemCount: 30,
-                itemBuilder: (BuildContext context, int index) {
-                  return SizedBox(
-                    height: 50,
-                    child: Center(child: Text('Item $index')),
-                  );
-                })));
+        body: NestedScrollView(headerSliverBuilder:
+            (BuildContext context, bool innerBoxIsScrolled) {
+      return <Widget>[
+        SliverOverlapAbsorber(
+          handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+          sliver: SliverAppBar(
+            title: const Text('Snapping Nested SliverAppBar'),
+            floating: true,
+            snap: true,
+            expandedHeight: 200.0,
+            forceElevated: innerBoxIsScrolled,
+          ),
+        ),
+      ];
+    }, body: Builder(builder: (BuildContext context) {
+      return CustomScrollView(
+// The "controller" and "primary" members should be left unset, so that the NestedScrollView can control this inner scroll view. If the "controller" property is set, then this scroll view will not be associated with the NestedScrollView.
+        slivers: <Widget>[
+          SliverOverlapInjector(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context)),
+          SliverFixedExtentList(
+            itemExtent: 48.0,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) =>
+                  ListTile(title: Text('Item $index')),
+              childCount: 30,
+            ),
+          ),
+        ],
+      );
+    })));
   }
 }

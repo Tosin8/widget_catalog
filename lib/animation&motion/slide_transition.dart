c83f8aa92044bc7d6b315a1d769f3f9395b97127
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 18:22:22 +0100

    Create slide_transition.dart

---
+++ lib/animation&motion/slide_transition.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Slide_Trans extends StatefulWidget {
+  const Slide_Trans({super.key});
+
+  @override
+  State<Slide_Trans> createState() => _Slide_TransState();
+}
+
+class _Slide_TransState extends State<Slide_Trans>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(1.5, 0.0),
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.elasticIn,
+  ));
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: const Padding(
+        padding: EdgeInsets.all(8.0),
+        child: FlutterLogo(size: 150.0),
+      ),
+    );
+  }
+}

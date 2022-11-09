f6ff8c6979fd716b19a6854075d5746cfc378aab
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 18:16:17 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'animation&motion/animated_widget.dart';
+import 'package:widget_catalog/animation&motion/decoratedBox_transition.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const animated_widget(),
+      home: const decorated_Box(),
     );
   }
 }

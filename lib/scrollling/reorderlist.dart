4d15ff3f925723300d3ee92f843080a7256f59a7
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 14:11:45 +0100

    Create ScrollBar dart file

--- lib/scrollling/reorderlist.dart
+++ lib/scrollling/reorderlist.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 // SHOWING BY DRAGGING THE USER CAN REORDER THE ITEMS OF THE LIST.
 // THE onREORDER parameter is required and wil be called when a child widget is dragged to a new posititon.
@@ -39,6 +40,67 @@ class _ReorderableExampleState extends State<ReorderableExample> {
             ),
         ],
       ),
+    );
+  }
+}
+
+// CHANGING THE PROXY ITEM'S BACKGROUND COLOR
+
+class ReorderableExample1 extends StatefulWidget {
+  const ReorderableExample1({super.key});
+
+  @override
+  State<ReorderableExample1> createState() => _ReorderableExample1State();
+}
+
+class _ReorderableExample1State extends State<ReorderableExample1> {
+  final List<int> _items = List<int>.generate(50, (int index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.secondary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.secondary.withOpacity(0.15);
+    final Color draggableItemColor = colorScheme.secondary;
+
+    Widget proxyDecorator(
+        Widget child, int index, Animation<double> animation) {
+      return AnimatedBuilder(
+        animation: animation,
+        builder: (BuildContext context, Widget? child) {
+          final double animValue = Curves.easeInOut.transform(animation.value);
+          final double elevation = lerpDouble(0, 6, animValue)!;
+          return Material(
+            elevation: elevation,
+            color: draggableItemColor,
+            shadowColor: draggableItemColor,
+            child: child,
+          );
+        },
+        child: child,
+      );
+    }
+
+    return ReorderableListView(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      proxyDecorator: proxyDecorator,
+      children: <Widget>[
+        for (int index = 0; index < _items.length; index += 1)
+          ListTile(
+            key: Key('$index'),
+            tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+            title: Text('Item ${_items[index]}'),
+          ),
+      ],
+      onReorder: (int oldIndex, int newIndex) {
+        setState(() {
+          if (oldIndex < newIndex) {
+            newIndex -= 1;
+          }
+          final int item = _items.removeAt(oldIndex);
+          _items.insert(newIndex, item);
+        });
+      },
     );
   }
 }
---
+++ lib/scrollling/scrollBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart'; 
+
+class ScrollingBar extends StatefulWidget {
+  const ScrollingBar({super.key});
+
+  @override
+  State<ScrollingBar> createState() => _ScrollingBarState();
+}
+
+class _ScrollingBarState extends State<ScrollingBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

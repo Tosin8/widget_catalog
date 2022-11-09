c73adae41abd4d18a864161894fcb8f9dc989d6d
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 15:15:47 +0100

    Update icon.dart

--- lib/assets,images&icon/icon.dart
+++ lib/assets,images&icon/icon.dart
@@ -1,2 +1,41 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
+// ICONBUTTON is used for interactive icon.
+
+class Icon_Widget extends StatelessWidget {
+  const Icon_Widget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Icon(
+              Icons.favorite,
+              color: Colors.pink,
+              size: 24,
+              semanticLabel: 'Text to announce in accesiblity modes',
+            ),
+            Icon(
+              Icons.audiotrack,
+              color: Colors.green,
+              size: 24,
+            ),
+            Icon(
+              Icons.umbrella,
+              color: Colors.blue,
+              size: 24,
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}

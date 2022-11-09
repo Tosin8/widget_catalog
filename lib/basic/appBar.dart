253dc3a05513132677d87087fb345209d684f8c4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 17:07:34 +0100

    Updating appbar

--- lib/basic/appBar.dart
+++ lib/basic/appBar.dart
@@ -7,6 +7,7 @@ class AppBarExample extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: const Icon(Icons.menu),
         title: const Text('AppBar Demo'),
         actions: <Widget>[
           IconButton(
@@ -44,6 +45,108 @@ class AppBarExample extends StatelessWidget {
         child: Text(
           'This is the home page',
           style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
+
+final List<int> _items = List<int>.generate(51, (int index) => index);
+
+class AppBarExample1 extends StatefulWidget {
+  const AppBarExample1({super.key});
+
+  @override
+  State<AppBarExample1> createState() => _AppBarExample1State();
+}
+
+class _AppBarExample1State extends State<AppBarExample1> {
+  bool shadowColor = false;
+  double? scrolledUnderElevation;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AppBar Demo'),
+        scrolledUnderElevation: scrolledUnderElevation,
+        shadowColor: shadowColor ? Theme.of(context).colorScheme.shadow : null,
+      ),
+      body: GridView.builder(
+        shrinkWrap: true,
+        itemCount: _items.length,
+        padding: const EdgeInsets.all(8.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 2.0,
+          mainAxisSpacing: 10.0,
+          crossAxisSpacing: 10.0,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Center(
+              child: Text(
+                'Scroll to see the Appbar in effect.',
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+          return Container(
+            alignment: Alignment.center,
+            // tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.0),
+              color: _items[index].isOdd ? oddItemColor : evenItemColor,
+            ),
+            child: Text('Item $index'),
+          );
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: OverflowBar(
+            overflowAlignment: OverflowBarAlignment.center,
+            alignment: MainAxisAlignment.center,
+            overflowSpacing: 5.0,
+            children: <Widget>[
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    shadowColor = !shadowColor;
+                  });
+                },
+                icon: Icon(
+                  shadowColor ? Icons.visibility_off : Icons.visibility,
+                ),
+                label: const Text('shadow color'),
+              ),
+              const SizedBox(width: 5),
+              ElevatedButton.icon(
+                onPressed: () {
+                  if (scrolledUnderElevation == null) {
+                    setState(() {
+                      // Default elevation is 3.0, increment by 1.0.
+                      scrolledUnderElevation = 4.0;
+                    });
+                  } else {
+                    setState(() {
+                      scrolledUnderElevation = scrolledUnderElevation! + 1.0;
+                    });
+                  }
+                },
+                icon: const Icon(Icons.add),
+                label: Text(
+                  'scrolledUnderElevation: ${scrolledUnderElevation ?? 'default'}',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const AppBarExample(),
+      home: const AppBarExample1(),
     );
   }
 }

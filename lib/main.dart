32b3c78751802bd64c49bc3b63b24b13cf5839f4
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 16:54:52 +0100

    Create AppBar dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'assets,images&icon/image.dart';
+import 'basic/appBar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const Image_Widget(),
+      home: const AppBarExample(),
     );
   }
 }

8b86adf0c64a6f5e5bcf133d7cb90d30c784ef02
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-09 14:41:02 +0100

    Updating scrollbar dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:widget_catalog/scrollling/reorderlist.dart';
+
+import 'scrollling/scrollBar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const ReorderableExample1(),
+      home: const ScrollingBar(),
     );
   }
 }
--- lib/scrollling/scrollBar.dart
+++ lib/scrollling/scrollBar.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
 class ScrollingBar extends StatefulWidget {
   const ScrollingBar({super.key});
@@ -8,8 +8,60 @@ class ScrollingBar extends StatefulWidget {
 }
 
 class _ScrollingBarState extends State<ScrollingBar> {
+  final ScrollController _firstController = ScrollController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        return Row(
+          children: [
+            SizedBox(
+                width: constraints.maxWidth / 2,
+                // When using the PrimaryScrollController and a Scrollbar
+                // together, only one ScrollPosition can be attached to the
+                // PrimaryScrollController at a time. Providing a
+                // unique scroll controller to this scroll view prevents it
+                // from attaching to the PrimaryScrollController.
+                child: Scrollbar(
+                    thumbVisibility: true,
+                    controller: _firstController,
+                    child: ListView.builder(
+                        controller: _firstController,
+                        itemCount: 100,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text('Scrollable 1: Index $index'),
+                          );
+                        }))),
+            SizedBox(
+                width: constraints.maxWidth / 2,
+                // This vertical scroll view has primary set to true, so it is
+                // using the PrimaryScrollController. On mobile platforms, the
+                // PrimaryScrollController automatically attaches to vertical
+                // ScrollViews, unlike on Desktop platforms, where the primary
+                // parameter is required.
+
+                child: Scrollbar(
+                    thumbVisibility: true,
+                    child: ListView.builder(
+                        primary: true,
+                        itemCount: 100,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                              height: 50,
+                              color: index.isEven
+                                  ? Colors.amberAccent
+                                  : Colors.blueAccent,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text('Scrollable 2: Index $index'),
+                              ));
+                        }))),
+          ],
+        );
+      }),
+    );
   }
 }

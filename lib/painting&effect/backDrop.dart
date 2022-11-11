9827d4a8c93492f7d2770585e31b1e86a48a315e
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-11 23:08:44 +0100

    Update backDrop.dart

--- lib/painting&effect/backDrop.dart
+++ lib/painting&effect/backDrop.dart
@@ -1,3 +1,36 @@
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 
-stfu
+// ignore: camel_case_types
+class BackDrop_Filter extends StatefulWidget {
+  const BackDrop_Filter({super.key});
+
+  @override
+  State<BackDrop_Filter> createState() => _BackDrop_FilterState();
+}
+
+class _BackDrop_FilterState extends State<BackDrop_Filter> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Stack(
+      fit: StackFit.expand,
+      children: [
+        Text('0' * 1000),
+        Center(
+          child: ClipRect(
+              child: BackdropFilter(
+                  filter: ui.ImageFilter.blur(
+                    sigmaX: 5.0,
+                    sigmaY: 5.0,
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 200.0,
+                    height: 200.0,
+                    child: const Text('Hello World'),
+                  ))),
+        )
+      ],
+    ));
+  }
+}

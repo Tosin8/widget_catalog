5b1fda3ae9b302cd2318b20568353877ea076186
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-11 23:51:12 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:widget_catalog/animation&motion/decoratedBox_transition.dart';
+
+import 'painting&effect/clipRect.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const decorated_Box(),
+      home: const Clip_Widget(),
     );
   }
 }

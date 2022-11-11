2642edd00e9222ecf99379ec3ab59546ce49dd2f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-11 22:48:15 +0100

    Create autoComplete.dart

---
+++ lib/input/autoComplete.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AutoComplete_Widg extends StatefulWidget {
+  const AutoComplete_Widg({super.key});
+
+  @override
+  State<AutoComplete_Widg> createState() => _AutoComplete_WidgState();
+}
+
+class _AutoComplete_WidgState extends State<AutoComplete_Widg> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

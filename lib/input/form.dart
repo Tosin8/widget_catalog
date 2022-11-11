e4b92a1d11459d862a3dc8e56c67cf32401cb105
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-11 22:12:13 +0100

    Create form.dart

---
+++ lib/input/form.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// DISCLAIMER: EACH INDIVIDUAL FORM FIELD SHOULB EBE WRAPPRED IN A FORM FIELD WIDGET.  - USING GLOBAL KEY TO ACHIEVE FORM STATE.
+
+class Form_Widget extends StatefulWidget {
+  const Form_Widget({super.key});
+
+  @override
+  State<Form_Widget> createState() => _Form_WidgetState();
+}
+
+class _Form_WidgetState extends State<Form_Widget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

6de5168f42582d029565f5c6eecd9b8977fb0796
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-12 22:08:08 +0100

    Update material_motion.dart

--- lib/codelab/material_motion.dart
+++ lib/codelab/material_motion.dart
@@ -1,3 +1,4 @@
-import 'package:flutter/material.dart'; 
 
 // 4 kinds of material motion
+// * CONTAINER TRANSFORM:  transitions between UI elements that include a container, creates a visible connection between two distinct UI elements by seemlessly transforming one element into another. 
+// * SHARED AXIS: transitions between UI elements that have a spartial or navigational relationships, uses a shared transformation on teh x,y or z axis to reinforce the relationship between elements. 

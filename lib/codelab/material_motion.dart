7d961e6df7f84a7c1b811b10fdb62a0232d09115
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-12 22:21:41 +0100

    Update material_motion.dart

--- lib/codelab/material_motion.dart
+++ lib/codelab/material_motion.dart
@@ -2,3 +2,5 @@
 // 4 kinds of material motion
 // * CONTAINER TRANSFORM:  transitions between UI elements that include a container, creates a visible connection between two distinct UI elements by seemlessly transforming one element into another. 
 // * SHARED AXIS: transitions between UI elements that have a spartial or navigational relationships, uses a shared transformation on teh x,y or z axis to reinforce the relationship between elements. 
+// * FADE THROUGH: transitions between UI elements that do not have a strong relationship to each othe; uses a sequentential fade out and fade in, with a scale of the incoming element. 
+// * FADE : used for UI elements that enter or exit within the bounds of the screen. 

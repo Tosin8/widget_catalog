daf91ff0b100d4a58d086adfc8d56150057236a9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-11-14 16:50:33 +0100

    update

--- lib/codelab/mdc basic.dart
+++ lib/codelab/mdc basic.dart
@@ -1,9 +1,8 @@
 // DISCLAIMER: This is an user interface for an mobile app login screen - named: Shrine.
 
-// MDC -101 , flutter, material components (mdc) basics. 
+// MDC -101 , flutter, material components (mdc) basics.
 
-import 'package:flutter/material.dart'; 
-
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -13,46 +12,64 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // adding text editing controllers .
 
-  // adding text editing controllers . 
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0), 
-          children: [
-            const SizedBox(height: 80.0), 
-            Column(
-              children: [
-                Image.asset('assets/diamond.png'), 
-                const SizedBox(height: 16.0), 
-                const Text('SHRINE'), 
-              ], 
-            ), 
-            const SizedBox(height: 120.0), 
+        body: SafeArea(
+            child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                children: [
+          const SizedBox(height: 80.0),
+          Column(
+            children: [
+              Image.asset('assets/diamond.png'),
+              const SizedBox(height: 16.0),
+              const Text('SHRINE'),
+            ],
+          ),
+          const SizedBox(height: 120.0),
 
-            // Adding: TextField Widgets
-            const TextField(
-              decoration: InputDecoration(
-                filled: true, 
-                labelText: 'Username', 
+          // Adding: TextField Widgets
+          TextField(
+            controller: _usernameController,
+            decoration: const InputDecoration(
+              filled: true,
+              labelText: 'Username',
+            ),
+          ),
+          // Spacer
+          const SizedBox(height: 12.0),
+          TextField(
+            controller: _passwordController,
+            decoration: const InputDecoration(
+              filled:
+                  true, // The filled: field means teh background of the text field is lightly filled in to help people recognize the tap or touch target area of the text field.
+              labelText: 'Password',
+            ),
+            obscureText:
+                true, // oscureText: true, value automatically replaces the input that the user types with bullets, which is appropriate  for password.
+          ),
+          // Adding: Button bar
+          OverflowBar(
+            alignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  _usernameController.clear();
+                  _passwordController.clear();
+                },
+                child: const Text('CANCEL'),
               ),
-            ), 
-            // Spacer
-            const SizedBox(height: 12.0), 
-            const TextField(
-              decoration: InputDecoration(
-                filled: true, // The filled: field means teh background of the text field is lightly filled in to help people recognize the tap or touch target area of the text field. 
-                labelText: 'Password', 
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('NEXT'),
               ),
-              obscureText: true, // oscureText: true, value automatically replaces the input that the user types with bullets, which is appropriate  for password. 
-            ), 
-            // Adding: Button bar
-          ]
-          )  )
-      ); 
-    
-
+            ],
+          )
+        ])));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'painting&effect/clipRect.dart';
+import 'package:widget_catalog/codelab/mdc%20basic.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: const Clip_Widget(),
+      home: const LoginPage(),
     );
   }
 }
